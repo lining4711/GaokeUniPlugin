468b65c1d4ecb5f6dd64fbcdaaa5930d24c70e3f
wangheng <thewayismile@163.com>
2025-08-14 22:28:41 +0800

    GKSC

---
+++ Base-Module/src/main/java/com/gksc/plugin/base/BaseModule.java
@@ -0,0 +1,20 @@
+package com.gksc.plugin.base;
+
+import android.widget.Toast;
+
+import io.dcloud.feature.uniapp.annotation.UniJSMethod;
+import io.dcloud.feature.uniapp.common.UniModule;
+
+public class BaseModule extends UniModule{
+
+    private static final String TAG = "FloatUniModule";
+
+    @UniJSMethod
+    public void test(){
+        showToast();;
+    }
+
+    private void showToast() {
+        Toast.makeText(mUniSDKInstance.getContext(), "原生插件调用成功", Toast.LENGTH_LONG).show();
+    }
+}
---
+++ app/src/main/java/con/gksc/plugin/MainActivity.java
@@ -0,0 +1,17 @@
+package con.gksc.plugin;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.gksc.plugin.R;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
